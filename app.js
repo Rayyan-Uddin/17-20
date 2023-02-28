74369255d19f0dacb867178f361f3bb2f71996b0
Rayyan-Uddin <rayyanuddin007@gmail.com>
2023-02-28 23:43:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+// Chapter 17 - 20
+
+// Question 1
+
+// var arr = [
+//     [1,2,3,4,5,6],
+//     [7,8,9,10,11,12],
+//     [13,14,15,16,17,18]
+// ]
+// document.write(arr)
+
+// -----------------------------------------
+// //  Question  2
+
+// var A = [0,1,2,3]
+// for(let a = 0 ; a<1; a++){
+//     console.log(A)
+// }
+// var B = [1,0,1,2]
+// for(let a = 0 ; a<1; a++){
+//     console.log(B)
+// }
+// var C = [2,1,0,1]
+// for(let a = 0 ; a<1; a++){
+//     console.log(C)
+// }
+
+// ----------------------------------------------------
+
+// // Question  3
+
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// -------------------------------------------------------
+
+// // Question  4
+
+// // var table = prompt("wirte the number");
+// var period = prompt("How long do you want to run this Table?")
+// // for(i = 1; i<= period ; i++){
+// //     document.write(table+" X" + i + " =" + i*table);
+// // }
+
+// -------------------------------------------------------------
+
+// // Question  5
+
+// var fruits = ["apple","banana","mangos","orange" ,"strawberry"]
+// for(let a = 0; a < fruits.length  ; a++){
+//     console.log(`element at index ${[a]} is ${fruits[a]}`)
+
+
+// }
+
+// ------------------------------------------------------------------
+
+// // Question  7
+// var orderOfItem = prompt("what do you wants")
+// var A = ["cake","apple pie","cookies","chips","patties"]
+// var flag = false;
+// for(let a = 0 ; a<A.length; a++){
+//     if(orderOfItem == A[a]){
+//           flag  = true
+//             boxNumber = a
+//     }
+// }
+// if(flag){
+//     console.log(`yes tis product ${orderOfItem} is aviable at the box number${boxNumber}`)
+
+// }else{
+//     console.log(`not aviable `)
+// }
+
+// var largestNumber = +prompt("Enter the largest number")
+// var Arrayitem = [24,53,78,91,12]
+// var m = 0
+// for(let a = 0 ; a<Arrayitem.length ; a++){
+//     if(Arrayitem[a] > m){
+
+//         m = Arrayitem[a]
+//         console.log(`the largest number is ${Arrayitem[a]}`)
+//     }
+// }
+
+// for(let i = 5; i<=100; i++){
+//     console.log(i)
+// }
+
+// -----------------------------------------------------------------
+
+// Question 8
+
+// var A = [24,53,78,92,12];
+// var largest = A[0];
+
+// for (var i=0 ; A.length > i ; i++){
+//     if (largest < A[i]){
+//         largest = A[i]
+//     }
+// }
+// document.write(largest)
+
+// -----------------------------------------------------------------
+
+// Question 9
+
+// var A = [24,53,78,92,12];
+// var smallest = A[0];
+
+// for (var i=0 ; A.length > i ; i++){
+//     if (smallest > A[i]){
+//         smallest = A[i]
+//     }
+// }
+// document.write(smallest)
+
+// ---------------------------------------------------------
+
+// Question 10
+
+// for (var i=1 ; i <= 20 ; i++)[
+//     a = (i * 5),
+//     document.write(a+",")
+// ]
